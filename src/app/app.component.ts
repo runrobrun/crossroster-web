fea59f76764105d9c94c3c3c2aca502fe374319a
Rob Hammann <rhammann@vermeer.com>
2021-08-27 13:39:44 -0500

    Adding user service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {UserService} from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crossroster-web';
+  constructor(public user: UserService) {
+
+  }
+
+  logout(): void {
+    this.user.logout();
+  }
+
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Router} from '@angular/router';
+import {map} from 'rxjs/operators';
+import {loggedIn} from '@angular/fire/auth-guard';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
+  pictureUrl$: Observable<string>;
+
+  constructor(private afAuth: AngularFireAuth,
+              private router: Router) {
+    this.isLoggedIn$ = afAuth.authState.pipe(map( user => !!user));
+    this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+    this.pictureUrl$ = afAuth.authState.pipe(map(user => user ? user.photoURL : null));
+  }
+
+  logout(): void {
+    this.afAuth.signOut();
+    this.router.navigateByUrl('/login');
+  }
+}
